ac1eb468c8a4a1fcca193fe7e5039a29c04c720d
Antti Roeyskoe <aroysko@paivola.fi>
2022-06-11 21:10:22 +0200

    Add template

---
+++ src/util/template.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+using pii = pair<int, int>
+using vi = vector<int>
+using ll = long long;
