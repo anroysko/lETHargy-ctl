3c8143d134c170144f9f44dcf07bc5841e243db1
yuhyao <827623970@qq.com>
2022-06-15 15:21:47 +0200

    Update hopcroft.cpp and undo-dsu.cpp

--- content/graph/undo-dsu.cpp
+++ content/graph/undo-dsu.cpp
@@ -1,32 +1,41 @@
-template<class T> struct Undoable_DSU
-{
-    vector<T> fa, sz;
-    vector<pair<T*, T>> sta;
-    Undoable_DSU(int n): fa(n)
-    {
-        iota(all(fa), 0);
-        sz.assign(n, 1);
-        sta.clear();
-    }
-    int getfa(int x)
-    {
-        T res = 0;
-        while(x != fa[x]) x = fa[x];
-        return x;
-    }
-    void safechg(T &x, T y) { sta.pb({&x, x}); x = y; }
-    bool merge(int x, int y)
-    {
-        int fx = getfa(x), fy = getfa(y);
-        if(fx == fy) return 1;
-        if(sz[fx] < sz[fy]) swap(fx, fy);
-        safechg(fa[fy], fx);
-        safechg(sz[fx], sz[fx] + sz[fy]);
-        return 0;
-    }
-    void get_top() { return sz(sta); }
-    void undo(int top)
-    {
-        while(sz(sta) > top) *sta.back().FI = sta.back().SE, sta.pop_back();
-    }
+/**
+ * Description: old = Use get_top() to get a save point; use undo(old) to go back to the save point.
+ * Time: O(\log N)
+ */
+
+template<class T> class Undo_DSU {
+private:
+	vector<T> fa, siz;
+	vector<pair<T*, T>> sta;
+
+	void safechg(T &x, T y) { sta.pb({&x, x}); x = y; }
+
+public:
+	Undo_DSU(int n): fa(n), siz(n, 1) {
+		iota(all(fa), 0);
+	}
+
+	int getfa(int x) {
+		T res = 0;
+		while (x != fa[x]) x = fa[x];
+		return x;
+	}
+
+	bool merge(int x, int y) {
+		int fx = getfa(x), fy = getfa(y);
+		if (fx == fy) return 1;
+		if (siz[fx] < siz[fy]) swap(fx, fy);
+		safechg(fa[fy], fx);
+		safechg(siz[fx], siz[fx] + siz[fy]);
+		return 0;
+	}
+
+	int get_top() { return sz(sta); }
+
+	void undo(int top) {
+		while (sz(sta) > top) {
+			*sta.back().first = sta.back().second;
+			sta.pop_back();
+		}
+	}
 };
