bf28838316ad4566384d82f3620412e5a58f19e5
yuhyao <827623970@qq.com>
2022-06-30 12:08:19 +0200

    Update modnum.cpp

--- content/numtheory/modnum.cpp
+++ content/numtheory/modnum.cpp
@@ -1,1 +1,31 @@
-// TODO
+/**
+ * Author: Yuhao Yao
+ * Ddescription: Modular integer. Note that there are several advantages to use this code:
+	1. You do not need to keep writing \% $mod$;
+	2. It is good to use this struct when doing Gaussian Elimination / Fast Walsh-Hadamard Transform;
+	3. Sometimes the input number is greater than $mod$ and using this code just handles it.
+ */
+#pragma once
+
+template<int mod> struct Z {
+	/// start-hash
+	int x;
+	Z(ll a = 0): x(a % mod) { if (x < 0) x += mod; }
+
+	static int norm(int x) { return x < 0 ? x + mod : x >= mod ? x - mod : x; }
+	friend Z operator +(Z a, Z b) { a.x = norm(a.x + b.x); return a; }
+	friend Z operator -(Z a, Z b) { a.x = norm(a.x - b.x); return a; }
+	friend Z operator *(Z a, Z b) { a.x = 1ll * a.x * b.x % mod; return a; }
+	/// end-hash
+
+	Z& operator +=(Z b) { x = norm(x + b.x); return *this; }
+	Z& operator -=(Z b) { x = norm(x - b.x); return *this; }
+
+	static Z power(Z a, ll k) { /// start-hash
+		Z res = 1;
+		for (; k; k >>= 1, a = a * a) if (k & 1) res = res * a;
+		return res;
+	}
+	friend Z operator /(Z a, Z b) { return a * power(b, mod - 2); }
+	/// end-hash
+};
