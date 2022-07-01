e9cf293451289be0554d089236d8af7c6799196a
yuhyao <827623970@qq.com>
2022-07-01 14:23:23 +0200

    Update ntt.cpp

--- content/math/ntt.cpp
+++ content/math/ntt.cpp
@@ -1,31 +1,31 @@
 /**
  * Author: Yuhao Yao
  * Description: Number Theoretic Transform. Use modnum as template parameter.
+ * Time: O(N \log N)
  */
 #pragma once
 
 template<class T> struct FFT {
 	const T g; // primitive root.
 	vi r;
-	int n2, len;
+	int n2;
 
 	FFT(): g(3) {}
 
 	void dft(vector<T> &a, int is_inv) { // is_inv == 1 -> idft.
 		rep(i, 1, n2 - 1) if (r[i] > i) swap(a[i], a[r[i]]);
-		rep(t, 0, len - 1) {
-			int step = 1 << t, step2 = step << 1;
+		for(int step = 1; step < n2; step <<= 1) {
 			vector<T> w(step);
-			T zeta = T::pow(g, (T::mod - 1) / step2);
+			T zeta = T::pow(g, (T::mod - 1) / (step << 1));
 			if (is_inv) zeta = 1 / zeta;
 
 			w[0] = 1;
 			rep(i, 1, step - 1) w[i] = w[i - 1] * zeta;
-			for (int i = 0; i < n2; i += step2) {
+			for (int i = 0; i < n2; i += step << 1) {
 				rep(j, 0, step - 1) {
 					T tmp = w[j] * a[i + j + step];
 					a[i + j + step] = a[i + j] - tmp;
-					a[i + j] = a[i + j] + tmp;
+					a[i + j] += tmp;
 				}
 			}
 		}
@@ -37,7 +37,7 @@ template<class T> struct FFT {
 	}
 	
 	void pre(int n) { // set n2, r; also used in polynomial inverse.
-		len = 0;
+		int len = 0;
 		for (n2 = 1; n2 < n; n2 <<= 1) len++;
 		r.resize(n2);
 		rep(i, 1, n2 - 1) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (len - 1));
