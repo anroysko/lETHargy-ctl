e6fb5165c548a43db08a4e9d05f1f9d55dcd35be
yuhyao <827623970@qq.com>
2022-07-04 03:48:56 +0200

    Update kmp.cpp

--- content/string/kmp.cpp
+++ content/string/kmp.cpp
@@ -5,29 +5,29 @@
  * Time: O(|p|) for precalculation and O(|p| + |t|) for matching.
  * Status: Not fully tested.
  */
-template<class T> class KMP {
-    int n;
-    T p;
-public:
-    vi fail;
-    KMP(const T &p): n(sz(p)), p(p), fail(n) {
-        int j = 0;
-        rep(i, 1, n - 1) {
-            while (j > 0 && p[j] != p[i]) j = fail[j - 1];
-            if (p[j] == p[i]) j++;
-            fail[i] = j;
-        }
-    }
+template<class T> struct KMP {
+	int n;
+	T p;
+	vi fail;
 
-    // gets all matched (starting) positions.
-    vi match(const T &t) {
-        int m = sz(t), j = 0;
-        vi res(m);
-        rep(i, 0, m - 1) {
-            while (j && (j == n || p[j] != t[i])) j = fail[j - 1];
-            if (p[j] == t[i]) j++;
-            if (j == n) res[i - n + 1] = 1;
-        }
-        return res;
-    }
+	KMP(const T &p): n(sz(p)), p(p), fail(n) {
+		int j = 0;
+		rep(i, 1, n - 1) {
+			while (j > 0 && p[j] != p[i]) j = fail[j - 1];
+			if (p[j] == p[i]) j++;
+			fail[i] = j;
+		}
+	}
+
+	// gets all matched (starting) positions.
+	vector<bool> match(const T &t) {
+		int m = sz(t), j = 0;
+		vector<bool> res(m);
+		rep(i, 0, m - 1) {
+			while (j && (j == n || p[j] != t[i])) j = fail[j - 1];
+			if (p[j] == t[i]) j++;
+			if (j == n) res[i - n + 1] = 1;
+		}
+		return res;
+	}
 };
