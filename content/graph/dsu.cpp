317c2101fe89fdbc15c415781807077153fa1273
yuhyao <827623970@qq.com>
2022-07-19 01:16:32 +0200

    Update dsu.cpp
    fix tab

--- content/graph/dsu.cpp
+++ content/graph/dsu.cpp
@@ -5,21 +5,21 @@
  */
 
 struct DSU {
-    vi fa, siz;
+	vi fa, siz;
 
-    DSU(int n): fa(n), siz(n, 1) { iota(all(fa), 0); }
-    
-    int getfa(int x) { return fa[x] == x ? x : fa[x] = getfa(fa[x]); }
+	DSU(int n): fa(n), siz(n, 1) { iota(all(fa), 0); }
+	
+	int getfa(int x) { return fa[x] == x ? x : fa[x] = getfa(fa[x]); }
 
-    bool check(int x, int y) { return getfa(x) != getfa(y); }
+	bool check(int x, int y) { return getfa(x) != getfa(y); }
 
-    // return 1 if x and y are in different component and merge.
-    bool merge(int x, int y) {
-        int fx = getfa(x), fy = getfa(y);
-        if (fx == fy) return 0;
-        if (siz[fx] < siz[fy]) swap(fx, fy);
-        fa[fy] = fx;
-        siz[fx] += siz[fy];
-        return 1;
-    }
+	// return 1 if x and y are in different component and merge.
+	bool merge(int x, int y) {
+		int fx = getfa(x), fy = getfa(y);
+		if (fx == fy) return 0;
+		if (siz[fx] < siz[fy]) swap(fx, fy);
+		fa[fy] = fx;
+		siz[fx] += siz[fy];
+		return 1;
+	}
 };
