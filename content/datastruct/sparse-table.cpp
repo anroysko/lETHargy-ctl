540af24703988abe8406efcab7e8b1b73f883490
yuhyao <827623970@qq.com>
2022-07-23 23:33:18 +0200

    add sparse table

---
+++ content/datastruct/sparse-table.cpp
@@ -0,0 +1,27 @@
+/**
+ * Author: Yuhao Yao
+ * Date: 22-07-23
+ * Description: Sparse Table.
+ * Time: O(N \log N) for construction, O(1) per query.
+ * Status: Tested on https://ac.nowcoder.com/acm/contest/33186/B. (better to test more)
+ */
+
+template<class T, class F = function<T(const T&, const T&)>>
+class SparseTable {
+	int n;
+	vector<vector<T>> st;
+	const F func;
+public:
+	SparseTable(const vector<T> &a, const F &f): n(sz(a)), func(f) {
+		assert(n > 0);
+		st.assign(__lg(n) + 1, vector<T>(n));
+		st[0] = a;
+		rep(i, 1, __lg(n)) rep(j, 0, n - (1 << i)) st[i][j] = func(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+	}
+
+	T ask(int l, int r) {
+		assert(0 <= l && l <= r && r < n);
+		int k = __lg(r - l + 1);
+		return func(st[k][l], st[k][r - (1 << k) + 1]);
+	}
+};
