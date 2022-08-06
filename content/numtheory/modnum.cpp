7d3be3a9c79a7f979c857f6eeed30844f94692fd
yuhyao <37280700+yuhyao@users.noreply.github.com>
2022-08-06 15:55:23 +0200

    Update modnum.cpp
    Modify it to fit Antti's previous code.

--- content/numtheory/modnum.cpp
+++ content/numtheory/modnum.cpp
@@ -1,10 +1,11 @@
 /**
  * Author: Yuhao Yao
+ * Date: 22-08-06
  * Description: Modular integer. Note that there are several advantages to use this code:
 	1. You do not need to keep writing \% $mod$;
 	2. It is good to use this struct when doing Gaussian Elimination / Fast Walsh-Hadamard Transform;
-	3. Sometimes the input number is greater than $mod$ and using this code just handles it.
- * Usage: mod should be a global variable (either const int or int).
+	3. Sometimes the input number is greater than $mod$ and this code handles it.
+ * Usage: $mod$ should be a global variable (either const int or int).
  */
 #pragma once
 
@@ -14,24 +15,23 @@ template<const int &mod_> struct Z {
 	int x;
 	Z(ll a = 0): x(a % mod) { if (x < 0) x += mod; }
 
-	static int norm(int x) { return x < 0 ? x + mod : x >= mod ? x - mod : x; }
-	friend Z operator +(Z a, Z b) { a.x = norm(a.x + b.x); return a; }
-	friend Z operator -(Z a, Z b) { a.x = norm(a.x - b.x); return a; }
-	friend Z operator *(Z a, Z b) { a.x = 1ll * a.x * b.x % mod; return a; }
+	Z& operator +=(Z b) { x += b.x; if (x >= mod) x -= mod; return *this; }
+	Z& operator -=(Z b) { x -= b.x; if (x < 0) x += mod; return *this; }
+	Z& operator *=(Z b) { x = 1ll * x * b.x % mod; return *this; }
+	friend Z operator +(Z a, Z b) { return a += b; }
+	friend Z operator -(Z a, Z b) { return a -= b; }
+	friend Z operator *(Z a, Z b) { return a *= b; }
 	/// end-hash
 
 	// the following are needed for fft and polynomial operations.
 	/// start-hash
-	Z& operator +=(Z b) { x = norm(x + b.x); return *this; }
-	Z& operator -=(Z b) { x = norm(x - b.x); return *this; }
-	Z& operator *=(Z b) { x = 1ll * x * b.x % mod; return *this; }
-
-	static Z pow(Z a, ll k) {
-		Z res = 1;
+	Z pow(ll k) const {
+		Z res = 1, a = *this;
 		for (; k; k >>= 1, a = a * a) if (k & 1) res = res * a;
 		return res;
 	}
-	friend Z operator /(Z a, Z b) { return a * pow(b, mod - 2); }
+	Z& operator /=(Z b) { *this = *this * b.pow(mod - 2); return *this; }
+	friend Z operator /(Z a, Z b) { return a /= b; }
 	/// end-hash
 
 	friend string to_string(Z a) { return to_string(a.x); } // just for debug.
