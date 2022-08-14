768be7933f226efedeee2e757c631a88b24c7401
yuhyao <37280700+yuhyao@users.noreply.github.com>
2022-08-14 09:53:22 +0200

    Update modnum.cpp
    change the static constexpr to a function getMod to obtain the mod

--- content/numtheory/modnum.cpp
+++ content/numtheory/modnum.cpp
@@ -1,18 +1,18 @@
 /**
  * Author: Yuhao Yao
- * Date: 22-08-13
+ * Date: 22-08-14
  * Description: Modular integer. Note that there are several advantages to use this code:
 	1. You do not need to keep writing \% $mod$;
 	2. It is good to use this struct when doing Gaussian Elimination / Fast Walsh-Hadamard Transform;
 	3. Sometimes the input number is greater than $mod$ and this code handles it.
  * Usage: $mod$ should be a global variable (either const int or int).
+  Do not write things like Mint{1 / 3}.pow(10) since 1 / 3 simply equals 0.
  * Status: tested on https://ac.nowcoder.com/acm/contest/33191/F.
  */
 #pragma once
 
-template<const int &mod_> struct Z {
+template<const int &mod> struct Z {
 	/// start-hash
-	static constexpr const int &mod = mod_;
 	int x;
 	Z(ll a = 0): x(a % mod) { if (x < 0) x += mod; }
 	explicit operator int() const { return x; }
@@ -25,7 +25,7 @@ template<const int &mod_> struct Z {
 	friend Z operator *(Z a, Z b) { return a *= b; }
 	/// end-hash
 
-	// the following are needed for fft and polynomial operations.
+	// the followings are needed for ntt and polynomial operations.
 	/// start-hash
 	Z pow(ll k) const {
 		Z res = 1, a = *this;
@@ -38,6 +38,8 @@ template<const int &mod_> struct Z {
 		return *this;
 	}
 	friend Z operator /(Z a, Z b) { return a /= b; }
+
+	static int getMod() { return mod; } // ntt need this.
 	/// end-hash
 
 	friend string to_string(Z a) { return to_string(a.x); } // just for debug.
