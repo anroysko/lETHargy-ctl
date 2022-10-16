47697b93b496d67510e78b6ecacd4f0f3a59531c
yuhyao <827623970@qq.com>
2022-10-16 03:43:06 +0200

    Update aho-corasick.cpp

--- content/string/aho-corasick.cpp
+++ content/string/aho-corasick.cpp
@@ -1,1 +1,55 @@
-// TODO
+/**
+ * Author: Yuhao Yao
+ * Date: 22-10-16
+ * Description: Aho_Corasick Automaton of strings $s_0, ..., s_{n - 1}$.
+ * Usage: Aho_Corasick<'a', 26> ac; for strings consisting of lowercase letters. Call ac.build() after you insert all strings $s_0, ..., s_{n - 1}$.
+ * Time: O(\sum_{i = 0}^{n - 1} |s_i|).
+ * Status: tested on http://ejudge.opencup.org/~ejudge/team.cgi?contest_id=2744 (problem M).
+ */
+template<char st, int C> struct Aho_Corasick {
+	struct node {
+		int nxt[C];
+		int fail;
+		int cnt;
+		node() {
+			memset(nxt, -1, sizeof nxt);
+			fail = -1;
+			cnt = 0;
+		}
+	};
+
+	vector<node> t;
+
+	Aho_Corasick(): t(1) {}
+
+	int insert(const string &s) {
+		int now = 0;
+		for (auto ch: s) {
+			int c = ch - st;
+			if (t[now].nxt[c] == -1) {
+				t.emplace_back();
+				t[now].nxt[c] = sz(t) - 1;
+			}
+			now = t[now].nxt[c];
+		}
+		t[now].cnt++;
+		return now;
+	}
+
+	void build() {
+		vi que{0};
+		rep(ind, 0, sz(que) - 1) {
+			int now = que[ind], fa = t[now].fail;
+			rep(c, 0, C - 1) {
+				int &v = t[now].nxt[c];
+				int u = fa == -1 ? 0 : t[fa].nxt[c];
+				if (v == -1) v = u;
+				else {
+					t[v].fail = u;
+					que.push_back(v);
+				}
+			}
+			if (fa != -1) t[now].cnt += t[fa].cnt;
+		}
+	}
+};
