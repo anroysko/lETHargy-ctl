4a53e944cb4d43d14c9d43b3bf7b16eefb795aae
yuhyao <827623970@qq.com>
2022-10-18 01:58:01 +0200

    Update Z3-vector.cpp

--- content/math/Z3-vector.cpp
+++ content/math/Z3-vector.cpp
@@ -1,44 +1,52 @@
 /**
  * Author: Yuhao Yao.
+ * Date: 22-10-18
  * Description: vector in $\mathbb{Z}_3$.
  * Time: O(L / w).
+ * Status: tested on https://codeforces.com/gym/102394/problem/G.
  */
 
 template<int L> struct v3 {
-    bitset<L> a[3];
-    v3() { a[0].set(); }
+	bitset<L> a[3]; /// start-hash
 
-    void set(int pos, int x) { rep(i, 0, 2) a[i][pos] = (i == x); }
-    int operator [](int i) const {
-        if (a[0][i]) return 0;
-        else if (a[1][i]) return 1;
-        else return 2;
-    }
-    v3 operator +(const v3 &rhs) const {
-        v3 res;
-        res.a[0] = (a[0] & rhs.a[0]) | (a[1] & rhs.a[2]) | (a[2] & rhs.a[1]);
-        res.a[1] = (a[0] & rhs.a[1]) | (a[1] & rhs.a[0]) | (a[2] & rhs.a[2]);
-        res.a[2] = (~res.a[0] & ~res.a[1]);
-        return res;
-    }
-    v3 operator -(const v3 &rhs) const {
-        v3 tmp = rhs;
-        swap(tmp.a[1], tmp.a[2]);
-        return operator +(tmp);
-    }
-    v3 operator *(int rhs) const {
-        if (rhs % 3 == 0) return v3{};
-        else {
-            auto res = *this;
-            if (rhs % 3 == 2) swap(res.a[1], res.a[2]);
-            return res;
-        }
-    }
-    v3 operator /(int rhs) const { assert(rhs % 3 != 0); return operator *(rhs); }
+	v3() { a[0].set(); }
 
-    friend string to_string(const v3 &a) {
-        string s;
-        rep(i, 0, L - 1) s.push_back('0' + a[i]);
-        return s;
-    }
+	void set(int pos, int x) {
+		rep(i, 0, 2) a[i][pos] = (i == x);
+	}
+	int operator [](int i) const {
+		if (a[0][i]) return 0;
+		else if (a[1][i]) return 1;
+		else return 2;
+	}
+	v3 operator +(const v3 &rhs) const {
+		v3 res;
+		res.a[0] = (a[0] & rhs.a[0]) | (a[1] & rhs.a[2]) | (a[2] & rhs.a[1]);
+		res.a[1] = (a[0] & rhs.a[1]) | (a[1] & rhs.a[0]) | (a[2] & rhs.a[2]);
+		res.a[2] = (~res.a[0] & ~res.a[1]);
+		return res;
+	}
+	v3 operator -(const v3 &rhs) const {
+		v3 tmp = rhs;
+		swap(tmp.a[1], tmp.a[2]);
+		return operator +(tmp);
+	}
+	v3 operator *(int rhs) const {
+		if (rhs % 3 == 0) return v3{};
+		else {
+			auto res = *this;
+			if (rhs % 3 == 2) swap(res.a[1], res.a[2]);
+			return res;
+		}
+	}
+	v3 operator /(int rhs) const {
+		assert(rhs % 3 != 0); 
+		return operator *(rhs);
+	} /// end-hash
+
+	friend string to_string(const v3 &a) {
+		string s;
+		rep(i, 0, L - 1) s.push_back('0' + a[i]);
+		return s;
+	}
 };
