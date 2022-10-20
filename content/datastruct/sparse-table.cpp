8424f287e0452d480e5b8e27e451da0c3dab1700
yuhyao <827623970@qq.com>
2022-10-20 12:37:02 +0200

    Update sparse-table.cpp

--- content/datastruct/sparse-table.cpp
+++ content/datastruct/sparse-table.cpp
@@ -1,9 +1,9 @@
 /**
  * Author: Yuhao Yao
- * Date: 22-07-23
+ * Date: 22-10-20
  * Description: Sparse Table.
  * Time: O(N \log N) for construction, O(1) per query.
- * Status: tested on https://ac.nowcoder.com/acm/contest/33186/B, https://qoj.ac/contest/695/problem/1857, https://codeforces.com/gym/103371/problem/M.
+ * Status: tested on https://qoj.ac/contest/695/problem/1857, https://codeforces.com/gym/103371/problem/M.
  */
 
 template<class T, class F = function<T(const T&, const T&)>>
@@ -12,11 +12,11 @@ class SparseTable {
 	vector<vector<T>> st;
 	const F func;
 public:
-	SparseTable(const vector<T> &a, const F &f): n(sz(a)), func(f) {
+	SparseTable(const vector<T> &init, const F &f): n(sz(init)), func(f) {
 		assert(n > 0);
 		st.assign(__lg(n) + 1, vector<T>(n));
-		st[0] = a;
-		rep(i, 1, __lg(n)) rep(j, 0, n - (1 << i)) st[i][j] = func(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+		st[0] = init;
+		rep(i, 1, __lg(n)) rep(x, 0, n - (1 << i)) st[i][x] = func(st[i - 1][x], st[i - 1][x + (1 << (i - 1))]);
 	}
 
 	T ask(int l, int r) {
