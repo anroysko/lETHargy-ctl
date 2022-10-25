4b5ea95bfbb5e69068250a2c315855b7ffacf488
yuhyao <827623970@qq.com>
2022-10-25 16:03:55 +0200

    Update max-clique.cpp

--- content/graph/max-clique.cpp
+++ content/graph/max-clique.cpp
@@ -4,7 +4,6 @@
  * Description: Finding a Maximum Clique of a graph $G = (V, E)$. Should be fine with $|V| \le 60$. (The algorithm actually enumberates all maximal clique, without double counting.)
  * Status: tested on https://judge.yosupo.jp/problem/maximum_independent_set, https://codeforces.com/contest/1578/problem/K.
  */
-
 template<int L>
 vi BronKerbosch(int n, const vector<pii> &es) {
 	using bs = bitset<L>;
